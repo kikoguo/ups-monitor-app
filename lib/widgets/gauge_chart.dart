64ec415e6eb1adeba26c8bf8252067b070a0aabd
kikoguo <kikoguo@github.com>
2026-05-20 16:27:37 +0800

    fix: 修复 GitHub Actions git config 命令格式错误

--- lib/widgets/gauge_chart.dart
+++ lib/widgets/gauge_chart.dart
@@ -33,7 +33,7 @@ class GaugeChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final percentage = (value / maxValue * 100).clamp(0.0, 100.0);
+    final percentage = (value / maxValue * 100).clamp(0.0, 100.0).toDouble();
 
     return Card(
       child: Padding(
